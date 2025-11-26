bd7ae3460863511f8c48678b4b803fa391243c18
Anish2581 <figmaani@gmail.com>
2025-11-26 14:38:37 +0000

    basic information about arrays and object

--- 2_basic/2_arrays.js
+++ 2_basic/2_arrays.js
@@ -7,15 +7,38 @@ const dc_heros=["superman","batman","flash"]
 // console.log(marvel_heros[3][2])//'flash'
 // console.log(((marvel_heros.join()).split(",")))//[ 'ironman', 'spiderman', 'hulk', 'superman', 'batman', 'flash' ]
 
-const all_heros=marvel_heros.concat(dc_heros)//push method will add element at the same array 
+// const all_heros=marvel_heros.concat(dc_heros)//push method will add element at the same array 
 //                                             while concat method will combine two array and return a new array
- console.log(all_heros)//[ 'ironman', 'spiderman', 'hulk', 'superman', 'batman', 'flash' ]
+//  console.log(all_heros)//[ 'ironman', 'spiderman', 'hulk', 'superman', 'batman', 'flash' ]
 
 
- const all_new_heros=[...marvel_heros,...dc_heros]
- console.log(all_new_heros)//[ 'ironman', 'spiderman', 'hulk', 'superman', 'batman', 'flash' ]
+//  const all_new_heros=[...marvel_heros,...dc_heros]
+//  console.log(all_new_heros)//[ 'ironman', 'spiderman', 'hulk', 'superman', 'batman', 'flash' ]
  //breaking glass method:- it is use "..." operator to break the arrays 
  // and it is "more sufficient as well as powerfull than "concat" method"
  
  //because in concat method we can add or merge only two arrays but 
  // in "breaking glass method" we can add or merge more than two arrays "
+
+
+ //flat :- it will flat the array according to layer mentioned in flat function
+ //        and return the new array 
+
+
+ let newarr3=[1,2,3,[3,5,6],[4,[3,2]]]
+ let store=newarr3.flat()//if no arrument is passed then its by default take "1"
+ console.log(newarr3)
+ console.log(store)
+store=newarr3.flat(2)//layer of flat =2
+store=newarr3.flat(Infinity)//layer of flat =2
+ console.log(store)
+
+ console.log(Array.isArray("Anish"))//varify wether is array or not
+ console.log(Array.from("Anish"))//convert any data into array
+
+ let score1=100 
+ let score2=200 
+ let score3=300 
+
+ console.log(Array.of(score1,score2,score3))//Returns a new array from a set of elements.
+
---
+++ 2_basic/3_object.js
@@ -0,0 +1,56 @@
+//singleton
+//object.create
+
+
+//object literal
+const sym=Symbol("23")
+const myobj={
+    name:"Anish",
+    "full name":"Anish Gupta",
+    age:18,
+    [sym]:"69",
+    location:"bandra",
+    email:"asd@gmail.com",
+    lastlogged:["saturday","sunday"],
+    isloggedIn:false
+}
+
+// console.log(myobj.name)//Anish
+// console.log(myobj["name"])//Anish
+
+// // console.log(myobj."full name")//error:- Unexpected string
+// console.log(myobj["full name"])// Anish Gupta
+
+// // console.log(myobj.sym)//69            //this will happen wan we take "sym" not "[sym]"
+// // console.log(typeof myobj.sym)//string //
+// //but we have to do symbol in object
+
+// console.log(myobj[sym])//69
+// console.log(typeof myobj[sym])//the key is a symbol and value is string 
+// //so whenever we try to print typeof "myobj[sym]" it will print type of value not key
+
+
+// myobj.email="anishg@gmail.com"
+// console.log(myobj.email)//obj are "mutable" means we can change after initialization
+
+// Object.freeze(myobj)//it will freeze or lock the object
+
+// myobj.email="jslearning.com"
+// console.log(myobj.email)//due to freeze an object myobj.email not change
+// console.log(myobj)
+
+myobj.newname="guptaji"//we can directly add any new variable in an object like this
+console.log(myobj)
+
+
+myobj.greeting= function(){
+    console.log("hello js user")
+}
+console.log(myobj.greeting)//[Function (anonymous)]
+console.log(myobj.greeting())//hello js user
+//"undefined" also print in console {we will discuss in upcoming topic}
+
+myobj.greeting2= function(){
+    console.log(`hello js user, ${this.name}`)//we can excess inside the object element it own elements using "this" keyword
+}
+console.log(myobj.greeting2())//hello js user, Anish
