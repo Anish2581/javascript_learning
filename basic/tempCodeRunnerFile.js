c9d3dfd32a01a2255d0fe56d2b1952eaa5b33b09
Anish2581 <figmaani@gmail.com>
2025-12-17 01:04:12 +0530

    frist commit on clone of this repository

--- basic/tempCodeRunnerFile.js
+++ basic/tempCodeRunnerFile.js
@@ -1,11 +1,1 @@
-const tinder={
-    email:"js@s.com",
-    username:{
-        fullname:{
-        firstname:"Anish",
-        lastname:"Gupta"
-        },
-    }
-}
-// console.log(tinder.username)
-console.log(tinder.username.fullname?.firstname)//"?" operator use when we are not sure it is present or not 
+console.log("hello i am making clone of this repository to code online")
